e5623bb0bf8c46fc7d3f90989231a00cbadf2b6b
jacky030607 <jacky030607@gmail.com>
2019-01-21 12:11:07 +0800

    modify config.php

--- config.php
+++ config.php
@@ -1,15 +1,21 @@
 <?php
+// your db server ip
 $DB_SERVER = "localhost";
+// your db name
 $DB_NAME = "apply-tool";
+// your db username
 $DB_USERNAME = "apply-tool";
+// your db password
 $DB_PASSWORD = "";
 
+//show errors. you should turn 'display_errors' on in php.ini too
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// connect to database. if errors occurred, display the error message.
 try{
-    $pdo_apply = new PDO("mysql:host=".$DB_SERVER.";dbname=".$DB_NAME,$DB_USERNAME,$DB_PASSWORD);
+    $pdo_apply = new PDO("mysql:host=".$DB_SERVER.";dbname=".$DB_NAME.";charset=utf8mb4",$DB_USERNAME,$DB_PASSWORD);
     $pdo_apply->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
     $pdo_apply->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }
