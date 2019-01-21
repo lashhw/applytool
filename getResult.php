c8cbc302ebf8dc5399c434a8208ead56ac44b4ae
jacky030607 <jacky030607@gmail.com>
2019-01-21 12:07:56 +0800

    add comment; implement color highlighting

--- getResult.php
+++ getResult.php
@@ -1,45 +1,79 @@
-<?php require "config.php" ?>
+<?php
+//config file
+require "config.php";
+
+// if 'qd' or 'qs' is empty, set it to '.*'(match any character(s))
+if($_GET["qd"]=="")$_GET["qd"]=".*";
+if($_GET["qs"]=="")$_GET["qs"]=".*";
+
+// convert id to school name
+function getSchoolName($id,$row,$pdo){
+  $stmt_school = $pdo->prepare("SELECT name FROM schoolData WHERE id=?");
+  // get the first three digits
+  $schoolid = substr($row["id"],0,3);
+  $stmt_school->execute(array($schoolid));
+  $schoolname = $stmt_school->fetchAll()[0]["name"];
+  return $schoolname;
+}
+
+//convert to formatted string
+$table_colors = array(
+  "頂標"=>"s1",
+  "前標"=>"s2",
+  "均標"=>"s3",
+  "後標"=>"s4",
+  "底標"=>"s5",
+  "採計"=>"s6",
+  "二階"=>"s7"
+);
+function getFormatted($str){
+  echo "<script>console.log('".$str."');</script>";
+  if(array_key_exists($str,$GLOBALS["table_colors"]))
+    return "<td class='".$GLOBALS["table_colors"][$str]."'>".$str."</td>";
+  else
+    return "<td class='td_default'></td>";
+}
+?>
 <div class="container">
   <?php
+  // serach for data whose name is regexly match
   $stmt_apply = $pdo_apply->prepare("SELECT id,name,chinese,math,english,society,science
                                FROM applyData WHERE name REGEXP ?;");
-  $stmt_apply->execute(array($_GET["q"]));
+  $stmt_apply->execute(array($_GET["qd"]));
   ?>
-  <table id="search_result" class="table table-striped">
-    <tr>
-      <th width="11%">學校</th>
-      <th width="20%">科系</th>
-      <th width="5%">國文</th>
-      <th width="5%">英文</th>
-      <th width="5%">數學</th>
-      <th width="5%">社會</th>
-      <th width="5%">自然</th>
-      <th width="7%">ID</th>
-    </tr>
+  <table id="table_result" class="table">
+    <thead class="thead-light">
+      <tr>
+        <th width="10%">學校</th>
+        <th width="15%">科系</th>
+        <th width="5%">國文</th>
+        <th width="5%">英文</th>
+        <th width="5%">數學</th>
+        <th width="5%">社會</th>
+        <th width="5%">自然</th>
+      </tr>
+    </thead>
+
+    <!-- loop through every rows which match regex and fliter schoolname -->
     <?php foreach ($stmt_apply->fetchAll() as $row):?>
-    <?php
-      $stmt_school = $pdo_apply->prepare("SELECT name FROM schoolData WHERE id=?");
-      $schoolid = substr($row["id"],0,3);
-      $stmt_school->execute(array($schoolid));
-      $schoolname = $stmt_school->fetchAll()[0]["name"];
-    ?>
-    <?php if(preg_match("/".$_GET["s"]."/",$schoolname)):?>
-    <tr>
-      <td><?php echo $schoolname?></td>
-      <td><?php echo $row["name"]?></td>
-      <td><?php echo $row["chinese"]?></td>
-      <td><?php echo $row["english"]?></td>
-      <td><?php echo $row["math"]?></td>
-      <td><?php echo $row["society"]?></td>
-      <td><?php echo $row["science"]?></td>
-      <td>
-      <a href=<?php echo "https://www.cac.edu.tw/apply108/system/108ColQry_forapply_3r5k9d/html/108_".$row["id"].".htm"?>
-        target="_blank">
-        <?php echo $row["id"]?>
-      </a>
-      </td>
-    </tr>
-    <?php endif;?>
+      <?php $schoolname = getSchoolName($row["id"],$row,$pdo_apply);?>
+      <?php if(preg_match("/".$_GET["qs"]."/",$schoolname)):?>
+      <tr>
+        <td class='td_default'><?php echo $schoolname?></td>
+        <td class='td_default'>
+          <!-- link to cac -->
+          <a href="<?php echo 'https://www.cac.edu.tw/apply108/system/108ColQry_forapply_3r5k9d/html/108_'.$row['id'].'.htm'?>"
+             target="_blank">
+            <?php echo $row["name"]?>
+          </a>
+        </td>
+        <?php echo getFormatted($row["chinese"])?>
+        <?php echo getFormatted($row["english"])?>
+        <?php echo getFormatted($row["math"])?>
+        <?php echo getFormatted($row["society"])?>
+        <?php echo getFormatted($row["science"])?>
+      </tr>
+      <?php endif;?>
     <?php endforeach;?>
   </table>
 </div>
