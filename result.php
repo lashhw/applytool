770b6107ce1c5f49bdead28adf0f0b045443ba7a
jacky030607 <jacky030607@gmail.com>
2019-01-23 18:29:27 +0800

    implement filter subject functionality

--- result.php
+++ result.php
@@ -1,7 +1,6 @@
 <?php
 // config file
 require "config.php";
-
 // if either qd or qs is not set, change it to empty string
 isset($_POST["qd"]) ? $qd=$_POST["qd"] : $qd="";
 isset($_POST["qs"]) ? $qs=$_POST["qs"] : $qs="";
@@ -14,6 +13,12 @@ else {
 // if 'qs' is empty, set it to '.*'(match any character(s))
   if($qs=="")$qs=".*";
 }
+
+$_POST["f1"]=="true" ? $f1="0" : $f1="1";
+$_POST["f2"]=="true" ? $f2="0" : $f2="1";
+$_POST["f3"]=="true" ? $f3="0" : $f3="1";
+$_POST["f4"]=="true" ? $f4="0" : $f4="1";
+$_POST["f5"]=="true" ? $f5="0" : $f5="1";
 
 //convert to formatted string
 $table_class = array(
@@ -41,19 +46,24 @@ function getFormatted($str){
     <tr>
       <th width="20%" class="align-middle">學校</th>
       <th width="30%" class="align-middle">科系</th>
-      <th width="10%" class="subject align-middle">國文</th>
-      <th width="10%" class="subject align-middle">英文</th>
-      <th width="10%" class="subject align-middle">數學</th>
-      <th width="10%" class="subject align-middle">社會</th>
-      <th width="10%" class="subject align-middle">自然</th>
+      <th width="10%" id="t1" class="subject align-middle">國文</th>
+      <th width="10%" id="t2" class="subject align-middle">英文</th>
+      <th width="10%" id="t3" class="subject align-middle">數學</th>
+      <th width="10%" id="t4" class="subject align-middle">社會</th>
+      <th width="10%" id="t5" class="subject align-middle">自然</th>
     </tr>
   </thead>
   <?php
   // search for data whose name and schoolname are regexly match
   $stmt_apply = $pdo_apply->prepare("SELECT id,name,chinese,math,english,society,science,school
-                                     FROM applyData WHERE name REGEXP ? AND school REGEXP ?;");
+                                     FROM applyData WHERE name REGEXP ? AND school REGEXP ?
+                                     AND (chinese='--' OR 1=?)
+                                     AND (english='--' OR 1=?)
+                                     AND (math='--' OR 1=?)
+                                     AND (society='--' OR 1=?)
+                                     AND (science='--' OR 1=?);");
   try{
-    $stmt_apply->execute(array($qd,$qs));
+    $stmt_apply->execute(array($qd,$qs,$f1,$f2,$f3,$f4,$f5));
   }
   catch(Exception $e){
     exit;
@@ -78,12 +88,3 @@ function getFormatted($str){
   </tr>
   <?php endforeach;?>
 </table>
-
-<script>
-if($(window).width()<768){
-  $(".subject").each(function(){
-    var str=$(this).text().substring(0,1);
-    $(this).text(str);
-  });
-}
-</script>
