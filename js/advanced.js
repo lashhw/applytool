f925e18821c2636eec43f4660efb0a77d705ffb2
jacky030607 <jacky030607@gmail.com>
2019-05-12 17:55:49 +0800

    rewrite code to follow JavaScript Standard Style

--- js/advanced.js
+++ js/advanced.js
@@ -1,7 +1,8 @@
-"use strict";
-var data = [];
-var results_per_query = 50;
-var subjects = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11'];
+/* eslint-disable camelcase */
+'use strict'
+var data = []
+var resultsPerQuery = 50
+var subjects = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11']
 var subjects_gsat = {
   's1': 2,
   's2': 2,
@@ -9,110 +10,114 @@ var subjects_gsat = {
   's4': 2,
   's5': 2,
   's6': 1
-};
+}
 
-function init(){
-  $('#table_result').floatThead();
+function init () {
+  $('#table_result').floatThead()
   $('#advanced_options').on('shown.bs.collapse hidden.bs.collapse', function () {
-    $('#table_result').floatThead('reflow');
-  });
-  $('[data-toggle="tooltip"]').tooltip();
+    $('#table_result').floatThead('reflow')
+  })
+  $('[data-toggle="tooltip"]').tooltip()
 }
 
-function getData(){
-  $.getJSON('data/advanced/data.json', function(json_data) {
-    for(var x in json_data)
-      data.push(json_data[x]);
-  });
-}
-
-function search(mode, qd, qs, start){
-  if(qd == '' && qs == '') return [false, []];
-
-  var results = [];
-  var cnt = 0;
-  for(var i=start; i<data.length; i++){
-    if(cnt == results_per_query) return [i, results];
-    var test = false;
-    if(mode=='2'){
-      var regex_qd = new RegExp(qd,'i');
-      var regex_qs = new RegExp(qs,'i');
-      test = regex_qd.test(data[i]['name']) && regex_qs.test(data[i]['school']);
+function getData () {
+  $.getJSON('data/advanced/data.json', function (jsonData) {
+    for (var x in jsonData) {
+      data.push(jsonData[x])
     }
-    else{
+  })
+}
+
+function search (mode, qd, qs, start) {
+  if (qd === '' && qs === '') return [false, []]
+
+  var results = []
+  var cnt = 0
+  for (var i = start; i < data.length; i++) {
+    if (cnt === resultsPerQuery) return [i, results]
+    var test = false
+    if (mode === '2') {
+      var regexQd = new RegExp(qd, 'i')
+      var regexQs = new RegExp(qs, 'i')
+      test = regexQd.test(data[i]['name']) && regexQs.test(data[i]['school'])
+    } else {
       test = data[i]['name'].toLowerCase().indexOf(qd.toLowerCase()) !== -1 &&
-             data[i]['school'].toLowerCase().indexOf(qs.toLowerCase()) !== -1;
+             data[i]['school'].toLowerCase().indexOf(qs.toLowerCase()) !== -1
     }
-    if(test){
-      results.push(data[i]);
-      cnt++;
+    if (test) {
+      results.push(data[i])
+      cnt++
     }
   }
-  return [false, results];
+  return [false, results]
 }
 
-function getFormatted(str, colspan, additional_class){
-  return "<td class='subject align-middle " + additional_class + "' colspan='" + colspan + "'>" + str + "</td>";
+function getFormatted (str, colspan, additionalClass) {
+  return "<td class='subject align-middle " + additionalClass + "' colspan='" + colspan + "'>" + str + '</td>'
 }
 
-function update_table(results){
-  var content = '';
-  for(var i=0; i<results.length; i++){
-    var url = "https://campus4.ncku.edu.tw/uac/cross_search/dept_info/" + results[i]['id'] + ".html";
-    content += '<tr>';
-    content += "<td class='align-middle' rowspan='2'>" + results[i]['school'] + '</td>';
-    content += "<td class='align-middle' rowspan='2'><a href='" + url + "' target='_blank'>" + results[i]['name'] + '</a></td>';
-    for(var j=0; j<subjects.length; j++){
-      if(subjects[j] in results[i]['subjects']){
-        if($(document).width()<768)
-          content += getFormatted("採", 1, 'advanced');
-        else
-          content += getFormatted('x'+results[i]['subjects'][subjects[j]], 1, 'advanced');
+function updateTable (results) {
+  var content = ''
+  for (var i = 0; i < results.length; i++) {
+    var url = 'https://campus4.ncku.edu.tw/uac/cross_search/dept_info/' + results[i]['id'] + '.html'
+    content += '<tr>'
+    content += "<td class='align-middle' rowspan='2'>" + results[i]['school'] + '</td>'
+    content += "<td class='align-middle' rowspan='2'><a href='" + url + "' target='_blank'>" + results[i]['name'] + '</a></td>'
+    for (var j = 0; j < subjects.length; j++) {
+      if (subjects[j] in results[i]['subjects']) {
+        if ($(document).width() < 768) {
+          content += getFormatted('採', 1, 'advanced')
+        } else {
+          content += getFormatted('x' + results[i]['subjects'][subjects[j]], 1, 'advanced')
+        }
+      } else {
+        content += getFormatted('--', 1, '')
       }
-      else
-        content += getFormatted('--', 1, '');
     }
-    content += '</tr><tr>';
-    for(var j in subjects_gsat) {
-      if(j in results[i]['subjects_gsat']){
-          if($(document).width()<768)
-            content += getFormatted(results[i]['subjects_gsat'][j].substring(0,1), subjects_gsat[j], 'gsat');
-          else
-            content += getFormatted(results[i]['subjects_gsat'][j], subjects_gsat[j], 'gsat');
+    content += '</tr><tr>'
+    for (var x in subjects_gsat) {
+      if (x in results[i]['subjects_gsat']) {
+        if ($(document).width() < 768) {
+          content += getFormatted(results[i]['subjects_gsat'][x].substring(0, 1), subjects_gsat[x], 'gsat')
+        } else {
+          content += getFormatted(results[i]['subjects_gsat'][x], subjects_gsat[x], 'gsat')
+        }
+      } else {
+        content += getFormatted('--', subjects_gsat[x], '')
       }
-      else
-        content += getFormatted('--', subjects_gsat[j], '');
     }
     content += '</tr>'
   }
-  $('#result_content').append(content);
-  $('#table_result').floatThead('reflow');
+  $('#result_content').append(content)
+  $('#table_result').floatThead('reflow')
 }
 
-function update(start){
-  if(typeof start === 'undefined'){
-    $('#result_content').empty();
-    start = 0;
+function update (start) {
+  if (typeof start === 'undefined') {
+    $('#result_content').empty()
+    start = 0
   }
-  var tmp = search($('input[name=mode]:checked').val(), $('#qd').val(), $('#qs').val(), start);
-  var next_start = tmp[0];
-  var results = tmp[1];
-  update_table(results);
-  if(next_start !== false) {
-    $('#more-results').removeClass('d-none')
-                      .off('click')
-                      .click(function(){
-                        update(next_start)
-                      });
-  }
-  else {
-    $('#more-results').addClass('d-none');
+  var tmp = search($('input[name=mode]:checked').val(), $('#qd').val(), $('#qs').val(), start)
+  var nextStart = tmp[0]
+  var results = tmp[1]
+  updateTable(results)
+  if (nextStart !== false) {
+    $('#more-results')
+      .removeClass('d-none')
+      .off('click')
+      .click(
+        function () {
+          update(nextStart)
+        }
+      )
+  } else {
+    $('#more-results').addClass('d-none')
   }
 }
 
 // get data and search automatically when the page is fully loaded
-$(document).ready(function(){
-  init();
-  getData();
-  update();
-});
+$(document).ready(function () {
+  init()
+  getData()
+  update()
+})
--- js/apply.js
+++ js/apply.js
@@ -1,109 +1,137 @@
-"use strict";
-var data = [];
-var subjects = ['chinese', 'english', 'math', 'society', 'science'];
-var subject_index = {
-  "頂標": "s1",
-  "前標": "s2",
-  "均標": "s3",
-  "後標": "s4",
-  "底標": "s5",
-  "採計": "s6",
-  "二階": "s7"
-};
-
-function getData(){
-  $.getJSON('data/apply/data.json', function(json_data) {
-    for(var x in json_data)
-      data.push(json_data[x]);
-  });
+'use strict'
+var data = []
+var subjects = [
+  'chinese',
+  'english',
+  'math',
+  'society',
+  'science'
+]
+var subjectIndex = {
+  '頂標': 's1',
+  '前標': 's2',
+  '均標': 's3',
+  '後標': 's4',
+  '底標': 's5',
+  '採計': 's6',
+  '二階': 's7'
 }
 
-function search(mode, qd, qs, c){
-  if(qd == '' && qs == '') return [];
+function getData () {
+  $.getJSON('data/apply/data.json', function (jsonData) {
+    for (var x in jsonData) data.push(jsonData[x])
+  })
+}
 
-  var results = [];
-  for(var i=0; i<data.length; i++){
-    var test = false;
-    if(mode=='2'){
-      var regex_qd = new RegExp(qd,'i');
-      var regex_qs = new RegExp(qs,'i');
-      test = regex_qd.test(data[i]['name']) && regex_qs.test(data[i]['school']);
-    }
-    else{
+function search (mode, qd, qs, c) {
+  if (qd === '' && qs === '') return []
+
+  var results = []
+  for (var i = 0; i < data.length; i++) {
+    var test = false
+    if (mode === '2') {
+      var regexQd = new RegExp(qd, 'i')
+      var regexQs = new RegExp(qs, 'i')
+      test = regexQd.test(data[i]['name']) && regexQs.test(data[i]['school'])
+    } else {
       test = data[i]['name'].toLowerCase().indexOf(qd.toLowerCase()) !== -1 &&
-             data[i]['school'].toLowerCase().indexOf(qs.toLowerCase()) !== -1;
+             data[i]['school'].toLowerCase().indexOf(qs.toLowerCase()) !== -1
     }
-    if(test){
-      var flag = true;
-      for(var j=0; j<5; j++){
-        if(data[i][subjects[j]]=='--' && c[j]=='2') flag = false;
-        if(data[i][subjects[j]]!='--' && c[j]=='3') flag = false;
+    if (test) {
+      var flag = true
+      for (var j = 0; j < 5; j++) {
+        if (data[i][subjects[j]] === '--' && c[j] === '2') flag = false
+        if (data[i][subjects[j]] !== '--' && c[j] === '3') flag = false
       }
-      if(flag) results.push(data[i]);
+      if (flag) results.push(data[i])
     }
   }
-  return results;
+  return results
 }
 
-function getFormatted(str){
-  if(str in subject_index){
-    return "<td class='subject align-middle " + subject_index[str] + "'>" + str + "</td>";
+function getFormatted (str) {
+  if (str in subjectIndex) {
+    return "<td class='subject align-middle " + subjectIndex[str] + "'>" + str + '</td>'
   }
-  return "<td class='subject align-middle'>" + str + "</td>";
+  return "<td class='subject align-middle'>" + str + '</td>'
 }
 
-function update(){
+function update () {
   // clear table
-  $('#result_content').empty();
-  var results = search($('input[name=mode]:checked').val(), $('#qd').val(), $('#qs').val(), [$('#sel1').val(),
-                                                                                             $('#sel2').val(),
-                                                                                             $('#sel3').val(),
-                                                                                             $('#sel4').val(),
-                                                                                             $('#sel5').val()]);
-  var content = '';
-  for(var i=0; i<results.length; i++){
-    var url = "https://www.cac.edu.tw/apply108/system/108ColQry_forapply_3r5k9d/html/108_" + results[i]['id'] + ".htm";
-    content += '<tr>';
-    content += '<td>' + results[i]['school'] + '</td>';
-    content += "<td><a href='" + url + "' target='_blank'>" + results[i]['name'] + '</a></td>';
-    for(var j=0; j<subjects.length; j++)
-      content += getFormatted(results[i][subjects[j]]);
-    content += '</tr>';
+  $('#result_content').empty()
+  var results = search(
+    $('input[name=mode]:checked').val(),
+    $('#qd').val(),
+    $('#qs').val(),
+    [
+      $('#sel1').val(),
+      $('#sel2').val(),
+      $('#sel3').val(),
+      $('#sel4').val(),
+      $('#sel5').val()
+    ]
+  )
+  var content = ''
+  for (var i = 0; i < results.length; i++) {
+    var url = 'https://www.cac.edu.tw/apply108/system/108ColQry_forapply_3r5k9d/html/108_' + results[i]['id'] + '.htm'
+    content += '<tr>'
+    content += '<td>' + results[i]['school'] + '</td>'
+    content += "<td><a href='" + url + "' target='_blank'>" + results[i]['name'] + '</a></td>'
+    for (var j = 0; j < subjects.length; j++) content += getFormatted(results[i][subjects[j]])
+    content += '</tr>'
   }
-  $('#result_content').append(content);
-  $('#table_result').floatThead('reflow');
+  $('#result_content').append(content)
+  $('#table_result').floatThead('reflow')
 
   // add strikethrough on subjects which is filtered
-  for(var i=1;i<=5;i++){
-    if($("#sel"+i).val() == '1')$("#t"+i).css({"text-decoration-line": "",
-                                               "text-decoration-color": "",
-                                               "color": ""});
-    if($("#sel"+i).val() == '2')$("#t"+i).css({"text-decoration-line": "underline",
-                                               "text-decoration-color": "green",
-                                               "color": "green"});
-    if($("#sel"+i).val() == '3')$("#t"+i).css({"text-decoration-line": "line-through",
-                                               "text-decoration-color": "darkred",
-                                               "color": "darkred"});
+  for (i = 1; i <= 5; i++) {
+    if ($('#sel' + i).val() === '1') {
+      $('#t' + i).css(
+        {
+          'text-decoration-line': '',
+          'text-decoration-color': '',
+          'color': ''
+        }
+      )
+    }
+    if ($('#sel' + i).val() === '2') {
+      $('#t' + i).css(
+        {
+          'text-decoration-line': 'underline',
+          'text-decoration-color': 'green',
+          'color': 'green'
+        }
+      )
+    }
+    if ($('#sel' + i).val() === '3') {
+      $('#t' + i).css(
+        {
+          'text-decoration-line': 'line-through',
+          'text-decoration-color': 'darkred',
+          'color': 'darkred'
+        }
+      )
+    }
   }
 }
 
 // get data and search automatically when the page is fully loaded
-$(document).ready(function(){
-  $('#table_result').floatThead();
+$(document).ready(function () {
+  $('#table_result').floatThead()
   $('#advanced_options').on('shown.bs.collapse hidden.bs.collapse', function () {
-    $('#table_result').floatThead('reflow');
-  });
-  if($(document).width()<768){
-    $("#mode-title").text('模式');
-    $(".subject-title").each(function(){
-      var str=$(this).text().substring(0,1);
-      $(this).text(str);
-    });
-    for(var i=1; i<=5;i++){
-      $('#sel'+i).removeClass('custom-select');
+    $('#table_result').floatThead('reflow')
+  })
+  if ($(document).width() < 768) {
+    $('#mode-title').text('模式')
+    $('.subject-title').each(function () {
+      var str = $(this).text().substring(0, 1)
+      $(this).text(str)
+    })
+    for (var i = 1; i <= 5; i++) {
+      $('#sel' + i).removeClass('custom-select')
     }
   }
-  $('[data-toggle="tooltip"]').tooltip();
-  getData();
-  update();
-});
+  $('[data-toggle="tooltip"]').tooltip()
+  getData()
+  update()
+})
