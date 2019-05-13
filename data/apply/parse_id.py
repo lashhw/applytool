74b3ff84dc9705673c58d57e9aa717e3f5dc883e
jacky030607 <jacky030607@gmail.com>
2019-05-13 21:05:27 +0800

    rewrite "Requests" section in parse_id.py

--- data/apply/parse_id.py
+++ data/apply/parse_id.py
@@ -1,23 +1,33 @@
 import requests, json, sys
 from bs4 import BeautifulSoup
+links = []
 data = []
-SCHOOL_START = 1
-SCHOOL_END = 154
 
-for schoolid in range(SCHOOL_START,SCHOOL_END+1):
-    schoolid_f = "{:03d}".format(schoolid)
-    print("processing "+schoolid_f+"...", end="")
+# get school url
+# Requests
+url1 = 'https://www.cac.edu.tw/apply108/system/108ColQry_forapply_3r5k9d/ShowSchool.php'
+r1 = requests.post(url1, data={'option': 'SCHNAME'})
+r1.encoding = 'utf-8'
+raw1 = r1.text
+# BeautifulSoup
+soup1 = BeautifulSoup(raw1, 'html.parser')
+for tag in soup1.find_all('a'):
+    links.append(tag.get('href'))
+
+for link in links:
+    print("processing %s..." % link, end="")
     sys.stdout.flush()
+
     # Requests
-    url = "https://www.cac.edu.tw/apply108/system/108ColQry_forapply_3r5k9d/ShowSchGsd.php?colno=%s" % schoolid_f
-    r = requests.get(url)
-    r.encoding = 'utf-8'
-    rawhtml = r.text
+    url2 = 'https://www.cac.edu.tw/apply108/system/108ColQry_forapply_3r5k9d/%s' % link
+    r2 = requests.get(url2, headers={'referer': url1})
+    r2.encoding = 'utf-8'
+    raw2 = r2.text
     # BeautifulSoup
-    soup = BeautifulSoup(rawhtml, 'html.parser')
-    allTags = soup.find_all('a')
-    for tag in allTags:
+    soup2 = BeautifulSoup(raw2, 'html.parser')
+    for tag in soup2.find_all('a'):
         data.append(tag['href'][-10:-4])
     print("ok!")
+
 f = open("id.json", "w")
 f.write(json.dumps(data))
