420ba172f591a20c2302bf9ba0cfbf6d53c706dd
jacky030607 <jacky030607@gmail.com>
2019-11-02 17:16:42 +0800

    update 109 advanced data

--- data/advanced/parse_id.py
+++ data/advanced/parse_id.py
@@ -1,24 +1,12 @@
-import requests, json, sys
-from bs4 import BeautifulSoup
-data = []
-GROUP_START = 1
-GROUP_END = 19
+import requests, json, js2py
 
-for groupid in range(GROUP_START,GROUP_END+1):
-    groupid_f = "{:02d}".format(groupid)
-    print("processing "+groupid_f+"...", end="")
-    sys.stdout.flush()
-    # Requests
-    url = "https://campus4.ncku.edu.tw/uac/cross_search/class_info/D%s.html" % groupid_f
-    r = requests.get(url)
-    r.encoding = 'utf-8'
-    rawhtml = r.text
-    # BeautifulSoup
-    soup = BeautifulSoup(rawhtml, 'html5lib')
-    allTags = soup.select('td.t > a')
-    for tag in allTags:
-        id = tag['href'][-10:-5]
-        if(id not in data): data.append(id)
-    print("ok!")
+link = "https://campus4.ncku.edu.tw/uac/cross_search/script/all_info.js"
+req = requests.get(link)
+js = req.text
+context = js2py.EvalJs()
+context.execute(js)
+all_data = context.all_dept_info
+data = [x[2] for x in all_data]
+
 f = open("id.json", "w")
 f.write(json.dumps(data))
